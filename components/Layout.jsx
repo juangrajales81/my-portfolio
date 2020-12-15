63604e4d6b3e901e62d8abf14a65a7d0ffea9761
juangrajales81 <juangrajales81@gmail.com>
2020-12-15 12:35:09 -0500

    First steps with Next.js

---
+++ components/Layout.jsx
@@ -0,0 +1,11 @@
+import Navbar from './shared/Navbar';
+
+const Layout = ({ children }) => (
+    <div>
+        <Navbar />
+        <div className="main-container container">
+            {children}
+        </div>
+    </div>
+)
+export default Layout;
